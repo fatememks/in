b4991ba63ec37d1e5d6bdbac2cc513dda992018d
[fatememks] <[fatemehmk0424@gmail.com]>
2024-08-07 08:53:32 -0400

    try

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { SiAkamai } from "react-icons/si";
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-400">fateme</div>
+      <SiAkamai />
+    </>
+  )
+}
+
+export default App
